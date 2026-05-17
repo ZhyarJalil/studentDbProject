d9d967b22a104494eaa94dfdfdf673aec2fd3ce3
ZhyarJalil <zhyarjalil98@gmail.com>
2026-05-17 09:19:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ with st.form("student_form", clear_on_submit=True):
     with col3:
         grade = st.text_input("Grade", placeholder="Third")
 
-    submit = st.form_submit_button("Add Student")
+    submit = st.form_submit_button("Add Student", help="")
     if submit:
         if name and major and grade:
             add_student(name, major, grade)
